af0a5d4cc079fc5b6172882b83ea940c6ac4f4c8
nabeen <watanabe_kenichiro@hasigo.co.jp>
2017-12-29 18:15:29 +0900

    add component

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+const App = () => (
+    <View>
+        <Header headerText={'WePic'} />
+        <AlbumList />
+    </View>
+);
 
 AppRegistry.registerComponent('WePic', () => App);
---
+++ src/components/AlbumList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class AlbumList extends Component {
+    componentWillMount() {
+        console.log('componentWillMount in AlbumList');
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>!!!</Text>
+            </View>
+        );
+    }
+}
+
+export default AlbumList;
